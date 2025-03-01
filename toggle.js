1f643028d4ddf5497008a9c5aef28d9606b7d41f
Ajewole-CODER <ajewolelemuel@gmail.com>
2025-03-01 21:52:18 +0000

    Add files via upload

--- toggle.js
+++ toggle.js
@@ -1,15 +1,31 @@
-const toggleSwitch = document.getElementById('toggleSwitch');
-const body = document.body;
-const toggleText = document.getElementById('toggleText');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+    const links = dropdownMenu.querySelectorAll('a');
+    const currentTheme = localStorage.getItem('theme');
 
-toggleSwitch.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
-
-    // Update toggle label based on current theme
-    if (body.classList.contains('dark-mode')) {
-        toggleText.textContent = 'Dark Mode';
-    } else {
-        toggleText.textContent = 'Light Mode';
+    if (currentTheme) {
+        document.body.classList.add(currentTheme + '-mode');
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add('dark-mode');
     }
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            document.body.classList.remove('light-mode', 'dark-mode');
+
+            const theme = link.getAttribute('data-theme');
+
+            if (theme === 'system') {
+                localStorage.removeItem('theme');
+                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    document.body.classList.add('dark-mode');
+                }
+            } else {
+                document.body.classList.add(theme + '-mode');
+                localStorage.setItem('theme', theme);
+            }
+        });
+    });
 });
